684cfebc437b09909b5dc76eeea24f515c2ec0c5
Melanie Matthews <melanie.a.matthews@gmail.com>
2019-09-02 14:05:49 +1000

    Go back to using postgres locally

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2019_08_26_022026) do
 
-  create_table "users", id: uuid, force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
